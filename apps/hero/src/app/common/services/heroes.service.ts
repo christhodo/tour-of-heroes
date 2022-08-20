182577210384bf245a6a6d04e7685215270e5185
christhodo <102981895+christhodo@users.noreply.github.com>
2022-08-20 07:34:20 -0700

    add server communication

--- apps/hero/src/app/common/services/heroes.service.ts
+++ apps/hero/src/app/common/services/heroes.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {
+  HeroEnvironment,
+  HERO_ENVIRONMENT,
+} from '@tour-of-heroes-app/environment';
+import { Hero } from '../models/hero';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://thirty-by-thirty-app.herokuapp.com';
 
 @Injectable({
   providedIn: 'root',
@@ -9,26 +14,29 @@ const BASE_URL = 'http://localhost:3000';
 export class HeroesService {
   model = 'heroes';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(HERO_ENVIRONMENT) private environment: HeroEnvironment
+  ) {}
 
   all() {
-    return this.http.get(this.getUrl());
+    return this.http.get<Hero[]>(this.getUrl());
   }
 
   find(id) {
-    return this.http.get(this.getUrlWithID(id));
+    return this.http.get<Hero>(this.getUrlWithID(id));
   }
 
   create(hero) {
-    return this.http.post(this.getUrl(), hero);
+    return this.http.post<Hero>(this.getUrl(), hero);
   }
 
   update(hero) {
-    return this.http.put(this.getUrlWithID(hero.id), hero);
+    return this.http.put<Hero>(this.getUrlWithID(hero.id), hero);
   }
 
   delete(id) {
-    return this.http.delete(this.getUrlWithID(id));
+    return this.http.delete<Hero>(this.getUrlWithID(id));
   }
 
   private getUrl() {
--- apps/hero/src/app/heroes/heroes.component.ts
+++ apps/hero/src/app/heroes/heroes.component.ts
@@ -51,7 +51,7 @@ export class HeroesComponent implements OnInit {
   }
 
   deleteHero(heroId) {
-    console.log('DELETE COURSE', heroId);
+    console.log('DELETE HERO', heroId);
   }
 
   reset() {
